7df4dd0e3ed53bd2190f53f78b45f450e77b884b
testy123-321 <testforthis123@gmail.com>
2026-06-01 12:51:43 +0200

    Delete smart-hospitality-&-sustainable-tourism-simulator directory

--- smart-hospitality-&-sustainable-tourism-simulator/src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://nmvplsjccchwduwtflki.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tdnBsc2pjY2Nod2R1d3RmbGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MjI2NzksImV4cCI6MjA5NTI5ODY3OX0.OKV1RDGczZfnRFiHwveqxa2PZI0LMcI4i8RKBIxlTEQ';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
